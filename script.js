fd0b9e8864ee478b2b0f0500302f116e03e7ba86
HarveeJain <harveej2006@gmail.com>
2025-12-24 19:32:42 +0530

    update image paths

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById("rollButton").addEventListener("click", function () {
     let diceValue = Math.floor(Math.random() * 6) + 1; // Random number from 1 to 6
-    document.getElementById("dice").src = `images/dice${diceValue}.png`; // Change image
+    document.getElementById("dice").src = `./images/dice${diceValue}.png`; // Change image
 });
